b5cd5da23183563812be1cecb0f1b6e57edc7966
Orlando Del Aguila <orlando@hashlabs.com>
2016-12-13 17:21:42 -0600

    adding vpc since it's require for t2 instances

--- main.tf
+++ main.tf
@@ -1,6 +1,35 @@
 /*
  * Resources
  */
+resource "aws_vpc" "default" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = "${aws_vpc.default.id}"
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags {
+    Name = "Public-a"
+  }
+}
+
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.default.id}"
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.default.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.default.id}"
+  }
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -20,6 +49,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "example" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.nano"
+  subnet_id     = "${aws_subnet.public_a.id}"
 
   tags {
     Name = "Terraform Intro"
@@ -29,6 +59,6 @@ resource "aws_instance" "example" {
 /*
  * Outputs
  */
-output "dns" {
-  value = "${aws_instance.example.public_dns}"
+output "ip" {
+  value = "${aws_instance.example.public_ip}"
 }
